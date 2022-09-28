d83780637299620b17be6baaca19f96b353a0a58
EdoPiz <109796739+EdoPiz@users.noreply.github.com>
2022-09-28 15:50:26 +0200

    dwada
    dwada

---
+++ 4.1.4 Inserimento di un periodo di servizio Orario Ridotto con tipo impiego "Supplenti della Scuola"/js/pillola_edge.js
@@ -0,0 +1,101 @@
+testi = new Array();
+testi[0]="Dalla pagina iniziale denominata \"Lista Richieste\" seleziona la voce \"Interrogazioni\", quindi \"Lista Rapporti di Lavoro\" ed infine \"Lista per Tipo Impiego ed Iscrizione\""
+testi[1]="Analizza i rapporti di lavoro: nel caso esemplificato in figura, si tratta di un insegnante che all'inizio della carriera ha lavorato come supplente"
+testi[2]="Scorri ulteriormente i rapporti di lavoro ed individua eventuali supplenze mancanti. In figura si ipotizza l'assenza di alcune supplenze svolte a settembre del 1997"
+testi[3]="Seleziona il menu \"Funzioni\" e quindi clicca sulla voce \"Inserimento Periodo di Servizio\""
+testi[4]="Clicca sul pulsante con il simbolo dei tre puntini (...) per inserire l'ente di appartenenza in cui è stato erogato il periodo di servizio"
+testi[5]="Nel campo \"Codice fiscale\" digita il CF del datore di lavoro. Nel caso di supplenza digita il CF del Ministero dell'Istruzione ossia \"80185250588\""
+testi[6]="Clicca sul pulsante \"Cerca\""
+testi[7]="Clicca sulla freccetta rossa per selezionare il datore di lavoro di interesse. Nel caso del Ministero dell'Istruzione deve essere selezionato il progressivo 00000"
+testi[8]="Digita nell'apposito campo, la \"Data inizio periodo\" della supplenza"
+testi[9]="Digita nell'apposito campo, la \"Data fine periodo\" della supplenza. La Data fine periodo deve appartenere allo stesso anno solare della Data inizio periodo"
+testi[10]="Nel periodo inserito in cui la Data fine periodo coincide con l'ultimo giorno di supplenza è fondamentale inserire il \"Motivo cessazione\""
+testi[11]="Clicca sulla scelta del \"Motivo Cessazione\" corrispondente a \"Fine incarico\""
+testi[12]="Seleziona la Gestione Pensionistica (C.T.P.S. per le supplenze). Per le supplenze non indicare la Gestione Previdenziale e il Regime di Fine Servizio"
+testi[13]="Clicca sul pulsante con il simbolo dei tre puntini (...) per inserire il Tipo Impiego"
+testi[14]="Clicca sulla scelta del Tipo Impiego corrispondente a \"Supplenti della Scuola\" (caso trattato nella presente pillola)"
+testi[15]="Clicca sul pulsante con il simbolo dei tre puntini (...) per inserire il \"Tipo Servizio\""
+testi[16]="Clicca sulla scelta corrispondente a \"Servizio ordinario\""
+testi[17]="Per l'orario ridotto, digita il numero di ore effettuate rispetto all'orario totale. In figura si ipotizzano 9 ore su un totale ore di 18"
+testi[18]="Per le supplenze post 31/12/1992, è necessario inserire le informazioni economiche: inserisci l'imponibile Pensionistico, inserisci l'imponibile Credito"
+testi[19]="Salva il periodo di servizio compilato ai passi precedenti cliccando sul pulsante \"Salva\""
+testi[20]="Clicca sul pulsante \"OK\" per confermare l'inserimento avvenuto"
+testi[21]="Se, nello stesso periodo, il docente ha prestato servizio presso un altro istituto, seleziona il menu \"Funzioni\" e clicca su \"Inserimento Periodo di Servizio\" "
+testi[22]="Clicca sul pulsante con il simbolo dei tre puntini (...) per inserire l'ente di appartenenza in cui è stato erogato il periodo di servizio"
+testi[23]="Nel campo \"Codice fiscale\" digita il CF del datore di lavoro. Nel caso di supplenza digita il CF del Ministero dell'Istruzione ossia \"80185250588\""
+testi[24]="Clicca sul pulsante \"Cerca\""
+testi[25]="Clicca sulla freccetta rossa per selezionare il datore di lavoro. Nel caso del Ministero dell'Istruzione, per questa seconda docenza sovrapposta, selezionare il progressivo 00001"
+testi[26]="Digita la \"Data inizio periodo\" e la \"Data fine periodo\" della supplenza. Clicca sui tre puntini (...) per inserire il \"Motivo cessazione\""
+testi[27]="Scegli la motivazione \"Fine incarico\" come motivo di cessazione"
+testi[28]="Scegli la Gestione Pensionistica (C.T.P.S. per le supplenze), il Tipo impiego \"Supplenti della Scuola\" ed il Tipo servizio \"Servizio ordinario\""
+testi[29]="Per l'orario ridotto, digita il numero di ore effettuate in questa supplenza rispetto all'orario totale. In figura si ipotizzano 6 ore su un totale di 18"
+testi[30]="Per tale supplenza post 31/12/1992, è necessario inserire le informazioni economiche: inserisci l'imponibile Pensionistico, inserisci l'imponibile Credito"
+testi[31]="Salva il periodo di servizio compilato ai passi precedenti cliccando sul pulsante \"Salva\""
+testi[32]="Clicca sul pulsante \"OK\" per confermare l'inserimento avvenuto"
+testi[33]="Se il docente, nello stesso periodo, ha lavorato in una terza Scuola, ripeti le operazioni selezionando, per l'ente di appartenenza, il progressivo 00002"
+testi[34]="Inserisci, anche nella terza supplenza, il Motivo Cessazione, il Tipo Impiego, il Tipo Servizio e la Gestione Pensionistica, l'imponibile Pensionistico e l'imponibile Credito"
+testi[35]="Per l'orario ridotto, digita il numero di ore effettuate in questa supplenza rispetto all'orario totale. In figura si ipotizzano 3 ore su un totale di 18"
+testi[36]="Clicca sul pulsante \"OK\" per confermare l'inserimento avvenuto"
+testi[37]="Per aprire il primo periodo clicca dove è posizionato il cursore"
+testi[38]="Per aprire il secondo periodo clicca dove è posizionato il cursore"
+testi[39]="Per aprire il terzo periodo clicca dove è posizionato il cursore"
+testi[40]="Clicca sui quadratini per esplodere le informazioni relative ai tre periodi e consultare, ad esempio, la retribuzione relativa alle tre supplenze inserite"
+testi[41]="La somma delle ore delle supplenze sovrapposte non deve superare il totale ore previsto. In caso contrario apparirà il messaggio bloccante rappresentato in figura."
+testi[42]="Fine Operazione"
+
+arrDetail = [];
+arrGiallo = [[0,3], [1,2], [2,2], [3,2], [4,2], [5,1], [6,3], [7,2], [8,2], [9,2], [10,2], [11,2], [12,2], [13,2], [14,2], [15,2], [16,2], [17,2], [18,2], [19,2], [20,2], [21,2], [22,2], [23,2], [24,2], [25,2], [26,2], [27,2], [28,3], [29,2], [30,2], [31,2], [32,2], [33,2], [34,10], [35,2], [36,2], [37,2], [38,2], [39,2], [40,2], [41,2]];
+arrFreccia = [0, 1, 2, 3, 4, 5, 6, 7];
+arrBgPlayerLarge = [6, 7];
+arrDisablePrevius = [6];
+
+function Stage_step_avanti(){
+
+	addButton();
+
+	var idx = chk("_avanti");
+
+	idx = indice;
+	console.log(idx);
+
+	var thisStep = strStep + idx;
+	var nextStep = strStep + ( parseInt(idx) + 1 ).toString();
+
+	details( idx );
+	disabledButton(idx);
+
+	$( "#Stage_step" + idx + "_freccia_immagine_freccia" ).fadeTo( delay , 1, function() {
+		switchStep( thisStep, nextStep, idx );
+		underlineTextAdd( idx );
+		hideObjPrevius(idx);
+		inside(idx);
+	});
+
+	showGiallo( thisStep, idx );
+
+	$("#indice").val(parseInt($("#indice").val()) + 1);
+	indice++;
+
+	return;
+}
+
+function underlineTextAdd( idx ) {
+	if ( idx == 6 ) {
+		$( "#Stage_step" + ( parseInt(idx) + 1 ) + "a_testo_passo" ).css("background-color", "rgb(211, 255, 233)").fadeTo( 1200 , 1, function() {
+			$( "#Stage_step" + ( parseInt(idx) + 1 ) + "a_testo_passo" ).css("background-color", "");
+		});
+	}
+}
+
+jQuery(function () {
+	jQuery(".Stage_step_page").find("div").each(function (index, obj) {
+		var offset = 2;
+		var topval = jQuery(obj).css("top");
+		topval = topval.replace("px", "");
+		if (topval == 0 || offset == 0)
+			return;
+
+		topval = parseInt(topval) + offset;
+		jQuery(obj).css("top", topval + "px");
+	})
+})
